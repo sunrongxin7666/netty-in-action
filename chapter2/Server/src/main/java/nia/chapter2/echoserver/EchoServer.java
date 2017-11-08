7da7e5b7e6b99f6f57021582ba3d06b7ff497715
sunrongxin7666 <sunrongxin7666@163.com>
2017-11-08 16:12:12 +0800

    init netty

--- chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
+++ chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
@@ -24,14 +24,14 @@ public class EchoServer {
 
     public static void main(String[] args)
         throws Exception {
-        if (args.length != 1) {
-            System.err.println("Usage: " + EchoServer.class.getSimpleName() +
-                " <port>"
-            );
-            return;
-        }
-        int port = Integer.parseInt(args[0]);
-        new EchoServer(port).start();
+//        if (args.length != 1) {
+//            System.err.println("Usage: " + EchoServer.class.getSimpleName() +
+//                " <port>"
+//            );
+//            return;
+//        }
+//        int port = Integer.parseInt(args[0]);
+        new EchoServer(8080).start();
     }
 
     public void start() throws Exception {
